c3b3dae2f641459d45b6a80f149f052f7edd7e2a
sininspira2 <37672276+sininspira2@users.noreply.github.com>
2025-09-02 01:10:24 -0400

    Delete drizzle/0002_easy_wendell_vaughn.sql

--- drizzle/0002_easy_wendell_vaughn.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE resources ADD `icon` text;--> statement-breakpoint
-ALTER TABLE resources ADD `status` text;--> statement-breakpoint
-ALTER TABLE resources ADD `target_quantity` integer;
